cf2097f5898afb667e7eeff4df669a2b9e1daa32
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-21 15:31:03 -0400

    added h1 heading displaying tic-tac-toe as game title at top loaded during init function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ let board = document.getElementById("board");
 
 window.addEventListener('load', init);
 
+function init () {
+    let heading = document.createElement("h1");
+    heading.textContent = "Tic-Tac-Toe";
+    board.appendChild(heading);
+}
