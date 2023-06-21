97ad51f3d214985c0ee7418c13317f775846d76e
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-21 15:40:20 -0400

    added more classlist to main heading and grid to align them to center

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ let board = document.getElementById("board");
 board.classList.add("text-primary");
 board.classList.add("container-fluid");
 board.classList.add("text-center");
+board.classList.add("d-flex");
+board.classList.add("flex-column")
+board.classList.add("align-items-center")
 
 window.addEventListener('load', init);
 
@@ -9,8 +12,15 @@ function init () {
     let heading = document.createElement("h1");
     heading.textContent = "Tic-Tac-Toe";
     board.appendChild(heading);
+
+    let grid = document.createElement("div");
+    grid.classList.add("border");
+    grid.classList.add("border-dark");
+    grid.classList.add("w-25");
+    board.appendChild(grid);
 }
 
 function createButtons() {
-
+    let button = document.createElement("button");
+    grid.appendChild()
 }
