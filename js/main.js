3ddde8299b9d0fab88f13d6ef8cb48782a753e6c
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-21 22:01:48 -0400

    created variable turns which is an array of 9 empty turns to be updated as the game is played

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ board.classList.add("flex-column")
 board.classList.add("align-items-center")
 
 let currentTurn;
+let turns = ["","","","","","","","",""];
+const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 //let playerXName = getPlayerXName();
 //let playerOName = getPlayerOName();
