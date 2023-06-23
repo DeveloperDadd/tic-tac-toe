aa3bef6fc83c0e0e7ad041d0283b801427426674
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-23 07:41:52 -0400

    working on debugging some functions and adding new variables

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ board.classList.add("align-items-center")
 
 let symbols = ["X", "O"];
 let currentTurn = 1;
+let currentSymbol = currentTurn % 2 !== 0 ? symbols[0] : symbols[1];
 let turns = [{player : null},{player : null},{player : null},{player : null},{player : null},{player : null},{player : null},{player : null},{player : null}];
 players = [{
     "symbol" : symbols[0],
@@ -30,20 +31,10 @@ function init () {
     board.appendChild(heading);
 
     createButtons();
-
-    let square1 = document.getElementById("square-1");
-    let square2 = document.getElementById("square-2");
-    let square3 = document.getElementById("square-3");
-    let square4 = document.getElementById("square-4");
-    let square5 = document.getElementById("square-5");
-    let square6 = document.getElementById("square-6");
-    let square7 = document.getElementById("square-7");
-    let square8 = document.getElementById("square-8");
-    let square9 = document.getElementById("square-9");
+    button.addEventListener('click', changeTurn);
 
     createResetButton();
     resetButton.addEventListener('click', resetGame);
-
 }
    
 function createButtons() {
@@ -79,7 +70,11 @@ function resetCounter() {
 }
 function resetGame() {
     resetTurns();
-    resetCounter;
+    resetCounter();
+}
+function changeTurn() {
+    button.textContent = currentSymbol;
+    currentTurn++;
 }
 
 
