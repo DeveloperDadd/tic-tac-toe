be46d2f2074a97d822dc0a7bf7429a895f1886b2
claytoncritchfield <clayton.critchfield@gmail.com>
2023-06-23 07:50:32 -0400

    changed turns back to an array of empty strings

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ board.classList.add("align-items-center")
 let symbols = ["X", "O"];
 let currentTurn = 1;
 let currentSymbol = currentTurn % 2 !== 0 ? symbols[0] : symbols[1];
-let turns = [{player : null},{player : null},{player : null},{player : null},{player : null},{player : null},{player : null},{player : null},{player : null}];
+let turns = ["","","","","","","","",""];
 
 const winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
